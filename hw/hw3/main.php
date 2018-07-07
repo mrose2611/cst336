36b6aad77fb13fcc30a329b76a51a6b6d1726813
Michael Rose <mirose@csumb.edu>
2018-07-07 04:45:10 +0000

    Updated hw3 with validation

--- hw/hw3/main.php
+++ hw/hw3/main.php
@@ -1,15 +1,31 @@
 <?php
     
-    if(!empty($_GET['sport'])) {
-        $sport = $_GET['sport'];
+    $sportError = $genderError = $colorsError = "";
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+        if(!empty($_POST['sport'])) {
+            $sport = $_POST['sport'];
+        }
+        else {
+            $sportError = "Please select a sport.";
+        }
+        
+        if(!empty($_POST['gender'])) {
+            $gender = $_POST['gender'];
+        }
+        else {
+            $genderError = "Please select a gender.";
+        }
+        
+        if(!empty($_POST['colors'])) {
+            $colors = $_POST['colors'];
+        }
+        else {
+            $colorsError = "Please select a color.";
+        }
+    
     }
-    if(!empty($_GET['gender'])) {
-        $gender = $_GET['gender'];
-    }
-    if(!empty($_GET['colors'])) {
-        $colors = $_GET['colors'];
-    }
-
 ?>
 
 <!DOCTYPE html>
@@ -30,7 +46,7 @@
         <p id= "instructions"><strong>Instructions:</strong> Pick a sport, gender, and some colors to view some shirts for that sport.  
         Note that there is only one shirt per gender/color combination.</p>
         <br>
-        <form>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <select name = "sport" style="color:black">
                 <option value = "">Select a Sport</option>
                 <option value = "bowling" <?= $sport == "bowling" ? "selected" : "" ?>>Bowling</option>
@@ -39,19 +55,23 @@
                 <option value = "tennis" <?= $sport == "tennis" ? "selected" : "" ?>>Tennis</option>
             </select>
             <br>
+            <span class="errors"><?php echo $sportError ?></span>
+            
             <div id="radioButtonDiv">
                 <fieldset class="selectors">
-                    <legend>Gender:</legend>
+                    <legend>Gender: <span class="required">*</span></legend>
                     <input type="radio" id="rmale" name="gender" value="male" <?= $gender == "male" ? "checked" : "" ?>>
                     <label for="rmale"> Male </label>
                     <br>
                     <input type="radio" id="rfemale" name="gender" value="female" <?= $gender == "female" ? "checked" : "" ?>>
                     <label for="rfemale"> Female </label>
+                    <br>
+                    <span class="errors"><?php echo $genderError ?></span>
                 </fieldset>
-                <br>
+
             </div>
             <fieldset class="selectors">
-                <legend>Color:</legend>
+                <legend>Color: <span class="required">*</span></legend>
                 <input type="checkbox" id="red" name="colors[]" value="red" <?= in_array("red",$colors) ? "checked" : "" ?>>
                 <label for="red" id="radioRed"> Red </label>
                 <input type="checkbox" id="green" name="colors[]" value="green" <?= in_array("green",$colors) ? "checked" : "" ?>>
@@ -60,7 +80,10 @@
                 <label for="blue" id="radioBlue"> Blue </label>
                 <input type="checkbox" id="white" name="colors[]" value="white" <?= in_array("white",$colors) ? "checked" : "" ?>>
                 <label for="white" id="radioWhite"> White/Grey </label>
+                <br>
+                <span class="errors"><?php echo $colorsError ?></span>
             </fieldset>
+
             <br>
             <input type="submit" id="submitButton" value="Show me some shirts!"/>
         </form>
