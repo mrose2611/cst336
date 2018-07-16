a1a649a467ada384552061744322586efc9f2d48
Michael Rose <mirose@csumb.edu>
2018-07-16 06:33:25 +0000

    updated lab 6

--- labs/lab6/login.php
+++ labs/lab6/login.php
@@ -7,16 +7,19 @@
     <head>
         <title> OtterMart Admin Page </title>
         <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+
     </head>
     
     <body>
         <h1> OtterMart Admin Page</h1>
         <img id= "csulogo" src="csumb_logo.jpg">
+        <div class="form-group row col-5">
         <form method="POST" action="loginProcess.php">
             Username: <input type="text" name="username"/> <br />
             Password: <input type="password" name="password"/> <br />
             
-            <input type="submit" name="submitForm" value="Login!" />
+            <input type="submit" class='btn btn-primary' name="submitForm" value="Login!" />
             <br /><br />
             <?php
                 if($_SESSION['incorrect']) {
@@ -25,5 +28,6 @@
                 }
             ?>
         </form>
+        </div>
     </body>
 </html>
